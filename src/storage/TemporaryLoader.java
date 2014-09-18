e66cb762a4700386c7acffa2fed93035639ec328
craigthelinguist <sinroth@gmail.com>
2014-09-18 20:56:30 +1200

    created some files i'll be making the world renderer prototype with

---
+++ src/storage/TemporaryLoader.java
@@ -0,0 +1,13 @@
+package storage;
+
+/**
+ * I'm just using this to load my tiles/other stuff until the real stuff comes along. You can safely
+ * ignore this class and it will be deleted before the end.
+ * @author craigthelinguist
+ */
+public class TemporaryLoader {
+
+	// TemporaryLoader is a singleton - you cannot instantiate it, only use its static methods.
+	private TemporaryLoader(){}
+	
+}
---
+++ src/world/AbstractTile.java
@@ -0,0 +1,10 @@
+package world;
+
+
+/**
+ * Represents one cell on the World. Different implementations of Tile might have different rules (e.g.:
+ * parties can stand on it, tile belongs to a city, so on).
+ */
+public abstract class AbstractTile {
+
+}
