061edf8af9a4c8bd724aa0ea8c6443d560b3d7a9
EwanMoshi <moshiewan@live.com>
2014-09-21 19:15:19 +1200

    added a custom border and set that border around the canvas

--- src/GUI/Canvas.java
+++ src/GUI/Canvas.java
@@ -6,7 +6,12 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import tools.GlobalConstants;
+import tools.ImageLoader;
 
 public class Canvas extends JPanel implements MouseListener{
 
@@ -16,6 +21,18 @@ public class Canvas extends JPanel implements MouseListener{
 		this.setBorder(BorderFactory.createLineBorder(Color.red)); //draws a border around canvas (just to show where the canvas is) (delete later)
 		mainFrame = frame;
 		this.setPreferredSize(new Dimension(frame.getWidth(),frame.getHeight()));
+		
+		/*Load the images for the border*/
+	    Border border = new Border(new ImageIcon(GlobalConstants.GUI_FILEPATH +"upperCenter.png").getImage(),
+		        new ImageIcon(GlobalConstants.GUI_FILEPATH +"upperLeft.png").getImage(), 
+		        new ImageIcon(GlobalConstants.GUI_FILEPATH +"upperRight.png").getImage(),
+		        new ImageIcon(GlobalConstants.GUI_FILEPATH +"leftCenter.png").getImage(),
+		        new ImageIcon(GlobalConstants.GUI_FILEPATH +"rightCenter.png").getImage(),
+		        new ImageIcon(GlobalConstants.GUI_FILEPATH +"bottomLeft.png").getImage(), 
+		        new ImageIcon(GlobalConstants.GUI_FILEPATH +"bottomCenter.png").getImage(),
+		        new ImageIcon(GlobalConstants.GUI_FILEPATH +"bottomRight.png").getImage());
+	    /*set the panel's border*/
+	    this.setBorder(border);
 	}
 	
 	
