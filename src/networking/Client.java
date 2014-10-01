fb2f658092f8bcb4aac0681aefb618461d710ef9
hartleneal <hartleneal@mei-kung.ecs.vuw.ac.nz>
2014-10-01 15:45:56 +1300

    Added some functionality

--- src/networking/Client.java
+++ src/networking/Client.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 /**
- * 
+ *
  * @author Neal Hartley
  *
  */
@@ -22,22 +22,25 @@ public class Client {
 
 		try {
 
-
+			System.out.println("'exit' to terminate connection.");
 			System.out.println("Send a message to the server.");
 
 
 			socket = new Socket("130.195.4.163", 45612);
 			printWriter = new PrintWriter(socket.getOutputStream(),true);
-			printWriter.println(message());
+			while(true) {
+				printWriter.println(message());
+			}
 		}
 		catch(Exception e) {
 			System.out.println(e);
 		}
 	}
-	
+
+
 	public static String message(){
-		String s = "error";
-		
+		String s = "exit";
+
 		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
 	    try {
 			s = bufferRead.readLine();
@@ -45,12 +48,25 @@ public class Client {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	    if(!s.equals("error")){
+
+	    if(!s.equals("exit")){
 		return s;
 	    }
-	    return "error!!!!";
+	    else{
+	    	try {
+				socket.close();
+				System.out.println("socket closed yo");
+				System.exit(0);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    	return "error!!!!";
+
+
+
+	    }
 	}
-	
 }
 
