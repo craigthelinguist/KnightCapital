f9c559ad448daef1e86ae5e2409ef87d5bbd01a2
hartleneal <hartleneal@mei-kung.ecs.vuw.ac.nz>
2014-10-01 14:54:45 +1300

    Just killing the client up in heaaa!

---
+++ src/networking/Client.java
@@ -0,0 +1,51 @@
+package networking;
+
+
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+public class Client {
+
+	private static Socket socket;
+	private static PrintWriter printWriter;
+
+	public static void main(String[] args) {
+
+		String s = "wrong!";
+
+		try {
+
+
+			System.out.println("Send a message to the server.");
+
+
+			socket = new Socket("130.195.4.163", 45612);
+			printWriter = new PrintWriter(socket.getOutputStream(),true);
+			printWriter.println(message());
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public static String message(){
+		String s = "error";
+		
+		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+	    try {
+			s = bufferRead.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	    if(!s.equals("error")){
+		return s;
+	    }
+	    return "error!!!!";
+	}
+	
+}
+
