77b61d63402d546264f122e1ef853cc4650d8978
hartleneal <hartleneal@mei-kung.ecs.vuw.ac.nz>
2014-10-01 15:02:24 +1300

    this ones mine too. Myles doesn't care about our game guys!

--- src/networking/Client.java
+++ src/networking/Client.java
@@ -6,6 +6,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+/**
+ * 
+ * @author Neal Hartley
+ *
+ */
 public class Client {
 
 	private static Socket socket;
