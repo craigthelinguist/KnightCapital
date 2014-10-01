f36d0565cd9286ed0b83d7fa53b6f553445346e2
hartleneal <hartleneal@mei-kung.ecs.vuw.ac.nz>
2014-10-01 16:21:56 +1300

    added ClientProtocol and Server protocol classes, for added usability in threading. Also me and Myles have one way chat from client to server working.

--- src/networking/Client.java
+++ src/networking/Client.java
@@ -25,11 +25,23 @@ public class Client {
 			System.out.println("'exit' to terminate connection.");
 			System.out.println("Send a message to the server.");
 
-
+            //connects to socket based on ip and port number. Ip needs to be configured for individual testing on different computers.
 			socket = new Socket("130.195.4.163", 45612);
+			
+			//printwriter that prints a message to the socket for the server to see.
 			printWriter = new PrintWriter(socket.getOutputStream(),true);
+			BufferedReader in =new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
 			while(true) {
 				printWriter.println(message());
+				
+				
+//			    if(in.readLine()!=null){
+//				System.out.println(in.readLine());
+//			    }
+				
+				
+				
 			}
 		}
 		catch(Exception e) {
@@ -37,21 +49,28 @@ public class Client {
 		}
 	}
 
-
+/**
+ * 
+ * @return message to send to server.
+ */
 	public static String message(){
 		String s = "exit";
 
+		//creates new buffered reader for reading user input
 		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
 	    try {
+	    	//assigns input to string.
 			s = bufferRead.readLine();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
-
+        
+	    // if not exiting returns the user input.
 	    if(!s.equals("exit")){
 		return s;
 	    }
+	    //otherwise it closes the socket
 	    else{
 	    	try {
 				socket.close();
