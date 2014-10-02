78dc86e4b9dddf4a2526d5eb1ec243dde61fa212
selemon <selemon3yitbarek@hotmail.com>
2014-10-03 10:48:11 +1300

    client implements runnable will be useful once we get the game going so that the client can recieve objects

--- src/networking/Client.java
+++ src/networking/Client.java
@@ -7,19 +7,30 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+
+import player.Player;
 /**
- *
+ *A client connection receives information about the current state of the game
+ * and relays that into the local copy of the game.
  * @author Neal Hartley and Myles Glass
  *
  */
-public class Client {
+public class Client implements Runnable {
 
 	private static Socket socket;
 	private static PrintWriter printWriter;
 
-	private static DataInputStream in;
+	private  static DataInputStream in;
 	private static DataOutputStream out;
+	
+	
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+	private int ID;
+	private Player player;
 
 	public static void main(String[] args) {
 
@@ -30,7 +41,7 @@ public class Client {
 			System.out.println("'exit' to terminate connection.");
 			System.out.println("Send a message to the server.");
 
-            //connects to socket based on ip and port number. Ip needs to be configured for individual testing on different computers.
+			//connects to socket based on ip and port number. Ip needs to be configured for individual testing on different computers.
 			System.out.println("connecting...");
 			socket = new Socket("130.195.4.155", 45612);
 			System.out.println("conected!");
@@ -62,43 +73,75 @@ public class Client {
 		}
 	}
 
-/**
- *
- * @return message to send to server.
- */
+	/**
+	 *
+	 * @return message to send to server.
+	 */
 	public static String message(){
 		String s = "exit";
 
 		//creates new buffered reader for reading user input
 		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-	    try {
-	    	//assigns input to string.
+		try {
+			//assigns input to string.
 			s = bufferRead.readLine();
 		} catch (IOException e) {
 
 			e.printStackTrace();
 		}
 
-	    // if not exiting returns the user input.
-	    if(!s.equals("exit")){
-		return s;
-	    }
-	    //otherwise it closes the socket
-	    else{
-	    	try {
-				socket.close();
-				System.out.println("socket closed yo");
-				System.exit(0);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		// if not exiting returns the user input.
+		if(!s.equals("exit")){
+			return s;
+		}
+		//otherwise it closes the socket
+		else{
+//			try {
+//				socket.close();
+//				System.out.println("socket closed yo");
+//				System.exit(0);
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
 
-	    	return "error!!!!";
+			return "error!!!!";
 
 
 
-	    }
+		}
+	}
+	
+	public Client(Socket socket) {
+		this.socket = socket;
+	}
+	
+	/**
+	 * receive player information from server
+	 * */
+	 
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			output = new ObjectOutputStream(socket.getOutputStream());
+			out.flush();
+			input = new ObjectInputStream(socket.getInputStream());
+			ID = in.readInt();
+			
+			player = (Player) input.readObject();
+			System.out.println("client request ID: "+ID);
+			
+			
+			
+			
+			
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
 
