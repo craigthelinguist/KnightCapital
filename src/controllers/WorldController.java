cd85b30aa4fe86b4563a262adf04b48f1bd54a0f
nealhartley <neal.w.hartley@gmail.com>
2014-10-03 13:54:17 +1300

    Integrated server and client into worldcontroller. Just temporary untill we can talk over how we want to structure it.

--- src/controllers/WorldController.java
+++ src/controllers/WorldController.java
@@ -5,13 +5,13 @@ import game.Hero;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.SwingUtilities;
 
-import GUI.MainFrame;
-
+import networking.Client;
+import networking.Server;
 import player.Player;
 import renderer.Camera;
 import renderer.WorldRenderer;
@@ -21,7 +21,7 @@ import tools.GlobalConstants;
 import world.Party;
 import world.Tile;
 import world.World;
-import world.WorldIcon;
+import GUI.MainFrame;
 
 /**
  * A WorldController. This is the glue between the model (World) and the view (gui, renderer).
@@ -29,19 +29,26 @@ import world.WorldIcon;
  */
 public class WorldController {
 
+	//boolean server or client, true if server, false if client.
+	private boolean serverOrClient = true;
+
+	//server and client.
+	private Server server;
+	private Client client;
+
 	// gui and renderer: the view
 	private MainFrame gui;
-	
+
 	// world this controller is for: the model
 	private final World world;
 
 	// player this controller belongs to
 	private Player player;
-	
+
 	// current tile and perspective
 	private Point selected;
 	private Camera camera;
-	
+
 	// key bindings
 	private static final int ROTATE_CW = KeyEvent.VK_R;
 	private static final int ROTATE_CCW = KeyEvent.VK_E;
@@ -49,9 +56,9 @@ public class WorldController {
 	private static final int PAN_DOWN = KeyEvent.VK_DOWN;
 	private static final int PAN_RIGHT = KeyEvent.VK_RIGHT;
 	private static final int PAN_LEFT = KeyEvent.VK_LEFT;
-	
+
 	public static Party ovelia;
-	
+
 	public WorldController(World w, Player p){
 		world = w;
 		ovelia = (Party) world.getTile(0,0).occupant();
@@ -60,36 +67,63 @@ public class WorldController {
 		gui = new MainFrame();
 		gui.setController(this);
 		selected = null;
+
+		if(serverOrClient){
+			try {
+				server = new Server();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		else {
+
+			client = new Client();
+
+		}
+
+
 	}
-	
+
 	/**
 	 * Player has pushed a key
 	 * @param ke: details about the key event
 	 */
 	public void keyPressed(KeyEvent ke){
-		
+
+
 		int code = ke.getKeyCode();
-		
+
 		if (code == ROTATE_CW){
 			camera.rotateClockwise();
+			notifier();
+			
 		}
 		else if (code == ROTATE_CCW){
 			camera.rotateCounterClockwise();
+			notifier();
 		}
 		else if (code == PAN_UP){
 			camera.panUp();
+			notifier();
 		}
 		else if (code == PAN_DOWN){
 			camera.panDown();
+			notifier();
 		}
 		else if (code == PAN_RIGHT){
 			camera.panRight();
+			notifier();
 		}
 		else if (code == PAN_LEFT){
 			camera.panLeft();
+			notifier();
 		}
-		gui.redraw();
-		
+		gui.redraw(); //redraws gui no matter what is pressed...inefficient much? cheers, Selemon.
+
+
+
 	}
 
 	/**
@@ -98,64 +132,70 @@ public class WorldController {
 	 * @param panel: what they clicked on (inventory, world, etc.)
 	 */
 	public void mousePressed(MouseEvent me, String panel){
-		
+
 		if (panel.equals("canvas")){
-		
+			
 
 			Point ptIso = new Point(me.getX(),me.getY());
 			Point ptRotated = Geometry.isometricToCartesian(ptIso, camera);
 			Point ptOriginal = Geometry.recoverOriginalPoint(ptRotated, camera, world);
-			
+
 			Tile clickedTile = world.getTile(ptOriginal);
 			Tile selectedTile = world.getTile(selected);
-			
+
 			// selected the tile
 			if (selectedTile != clickedTile && SwingUtilities.isLeftMouseButton(me)){
 				selected = ptOriginal;
 				gui.updateInfo(clickedTile);
 			}
-			
+
 			// deselected the tile
 			else if (selected != null && SwingUtilities.isLeftMouseButton(me)){
 				if (selectedTile == clickedTile) selected = null;
 				gui.updateInfo(null);
 			}
-		
+
 			// moved
 			else if (selected != null && SwingUtilities.isRightMouseButton(me)){
 				boolean moved = world.moveParty(player, selected, ptOriginal);
 				if (moved){
 					selected = ptOriginal;
-					gui.updateInfo(clickedTile);					
+					gui.updateInfo(clickedTile);
 				}
 			}
-		
+
 		}
-		
+
 		gui.redraw();
-		
+
 	}
-	
+
 	/**
 	 * Player has clicked a button.
 	 * @param button: the button they clicked.
 	 */
 	public void buttonPressed(JButton button){
-		
+
 	}
-	
+
 	public World getWorld(){
 		return world;
 	}
-	
+
 	public Camera getCamera(){
 		return camera;
 	}
-	
+
 	public Tile getSelectedTile(){
 		return world.getTile(selected);
 	}
 	
+	public void notifier(){
+		
+		if(client!=null)client.notifyThread();
+		if(client == null)System.out.println("still not initiated");
+	}
+
 	public static void main(String[] args){
 		World w = TemporaryLoader.loadWorld("world_temporary.txt");
 		Player p = new Player();
@@ -167,5 +207,5 @@ public class WorldController {
 		w.getTile(0,0).setIcon(party);
 		new WorldController(w,p);
 	}
-	
+
 }
--- src/networking/ClientProtocol.java
+++ src/networking/ClientProtocol.java
@@ -6,54 +6,65 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.EOFException;
 import java.io.InputStreamReader;
+
 /**
- *
- * @author Neal Hartley
- * Runnable thread implemented by the client in order to listen to responses form the server port.
- * Will hold functionality to deal with different types of incoming messages and how they will
- * affect the clients current board state.
+ * 
+ * @author Neal Hartley Runnable thread implemented by the client in order to
+ *         listen to responses form the server port. Will hold functionality to
+ *         deal with different types of incoming messages and how they will
+ *         affect the clients current board state.
  */
-public class ClientProtocol implements Runnable{
+public class ClientProtocol implements Runnable {
 
 	private static DataInputStream in;
 	private boolean clean = true;
 	private static DataOutputStream out;
 
+	private boolean changed = true;
+
 	/**
-	 * needs to be initialised with a DataInputStream from an already initialised port
-	 * Stream is constantly checked for incoming messages.
+	 * needs to be initialised with a DataInputStream from an already
+	 * initialised port Stream is constantly checked for incoming messages.
+	 * 
 	 * @param in
 	 */
-	public ClientProtocol(DataInputStream in, DataOutputStream out){
+	public ClientProtocol(DataInputStream in, DataOutputStream out) {
 		this.in = in;
 		this.out = out;
 	}
 
-
-
 	/**
-	 * consistently checks for incoming data from socket, deals with it accordingly.
+	 * consistently checks for incoming data from socket, deals with it
+	 * accordingly.
 	 */
 	@Override
 	public void run() {
 
 		String message;
 
+		while (clean) {
+			System.out.println("clientProtocol running");
 
-		while(clean){
-			 System.out.println("clientProtocol running");
+			if (changed) {
 
-
+				try {
+					out.writeUTF("a change has occured");
+					changed = false;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 
 			try {
-				// currently checks for incoming string messages, functionality for
+				// currently checks for incoming string messages, functionality
+				// for
 				// reading byte stream for board changes will be added.
 				message = in.readUTF();
 				System.out.println("global message: " + message);
-			}
-			catch (EOFException e) {
+			} catch (EOFException e) {
 				this.clean = false;
-				}
+			}
 
 			catch (IOException e) {
 
@@ -62,11 +73,15 @@ public class ClientProtocol implements Runnable{
 		}
 	}
 
-
-	public Boolean getStatus(){
+	public Boolean getStatus() {
 		return clean;
 
 	}
 
+	public void notifyChange() {
+		System.out.println("change has been notified!");
+		this.changed = true;
+
+	}
 
 }
