6a2009a765c834b3287e5d52d91822023fe09541
Ewan Moshi <ewanmoshi@Live.com>
2014-10-03 13:19:29 +1300

    hovering over items lightens the image.

--- src/GUI/InventoryPanel.java
+++ src/GUI/InventoryPanel.java
@@ -19,10 +19,11 @@ import javax.swing.JPanel;
 
 import tools.GlobalConstants;
 import tools.ImageLoader;
+import tools.ImageManipulation;
 
 /**
  * This panel displays inventory as an image background for the panel
- * 
+ *
  * @author Ewan Moshi
  *
  */
@@ -35,30 +36,30 @@ public class InventoryPanel extends JPanel implements MouseListener {
 	GameDialog dSlot4;
 	GameDialog dSlot5;
 	GameDialog dSlot6;
-	
+
 	/*The main frame */
 	private MainFrame frame;
-	
+
 	/*The item slots*/
 	ItemSlotPanel slot1;
-	JPanel slot2;
+	ItemSlotPanel slot2;
 	JPanel slot3;
 	JPanel slot4;
 	JPanel slot5;
 	JPanel slot6;
-	
+
 	private BufferedImage backgroundImage;
-	
+
 	public InventoryPanel(MainFrame f) {
 		this.frame = f;
-		
+
 		/*set the size of this panel to be size of the image*/
 		this.setPreferredSize(new Dimension(375,200));
 		this.setOpaque(true);
 		/*Initialize the image for the inventory panel*/
-		backgroundImage = ImageLoader.load(GlobalConstants.GUI_FILEPATH + "inventory.png");	
-		
-		
+		backgroundImage = ImageLoader.load(GlobalConstants.GUI_FILEPATH + "inventory.png");
+
+
 		/*Initialize the layout and the insets*/
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
@@ -67,22 +68,22 @@ public class InventoryPanel extends JPanel implements MouseListener {
 		c.fill = GridBagConstraints.BOTH;
 		c.weightx = 1.0;
 		c.weighty = 1.0;
-		
+
 		/*The top row panels */
-		
+
 		/*Declare and initialize slot1 (panel) */
 		slot1 = new ItemSlotPanel("epipenSlot.png");
 		c.insets = new Insets(15,15,10,10);
 		c.gridx = 0;
 		c.gridy = 0;
 		this.add(slot1,c);
-		
+
 		/*Declare and initialize slot2 */
 		slot2 = new ItemSlotPanel("potionSlot.png");
 		c.gridx = 1;
 		c.gridy = 0;
 		this.add(slot2,c);
-		
+
 		/*Declare and initialize slot3 */
 		slot3 = new JPanel();
 		slot3.setBackground(new Color(255,0,0,50));  //this is just to test the position of this panel (third parameter is alpha value)
@@ -90,10 +91,10 @@ public class InventoryPanel extends JPanel implements MouseListener {
 		c.gridx = 2;
 		c.gridy = 0;
 		this.add(slot3,c);
-		
-		
+
+
 		/*The bottom row panels*/
-		
+
 		/*Declare and initialize slot4 */
 		slot4 = new JPanel();
 		slot4.setBackground(new Color(255,0,0,50));
@@ -101,14 +102,14 @@ public class InventoryPanel extends JPanel implements MouseListener {
 		c.gridx = 0;
 		c.gridy = 1;
 		this.add(slot4,c);
-		
+
 		/*Declare and initialize slot5 */
 		slot5 = new JPanel();
 		slot5.setBackground(new Color(255,0,0,50));
 		c.gridx = 1;
 		c.gridy = 1;
 		this.add(slot5,c);
-		
+
 		/*Declare and initialize slot6 */
 		slot6 = new JPanel();
 		slot6.setBackground(new Color(255,0,0,50));
@@ -116,7 +117,7 @@ public class InventoryPanel extends JPanel implements MouseListener {
 		c.gridx = 2;
 		c.gridy = 1;
 		this.add(slot6,c);
-		
+
 		/*Add mouse listener for all slots */
 		slot1.addMouseListener(this);
 		slot2.addMouseListener(this);
@@ -124,11 +125,11 @@ public class InventoryPanel extends JPanel implements MouseListener {
 		slot4.addMouseListener(this);
 		slot5.addMouseListener(this);
 		slot6.addMouseListener(this);
-			
-		
+
+
 	}
-		
-		
+
+
 	  @Override
 	  protected void paintComponent(Graphics g) {
 	    super.paintComponent(g);
@@ -144,58 +145,62 @@ public class InventoryPanel extends JPanel implements MouseListener {
 	public void mouseEntered(MouseEvent e) {
 		repaint();
 		if(e.getSource() == slot1) {
+			slot1.changeBackground(ImageManipulation.lighten(slot1.getBackgroundImage()));
 			dSlot1 = new ItemSlotInformation(frame,"Epinephrine Autoinjector. Used for intramuscular injection to revive unconscious casualties.");
 			dSlot1.setLocation(frame.getWidth()-500,frame.getHeight()-400);
 			dSlot1.setVisible(true);
 	    }
 		if(e.getSource() == slot2) {
+			slot2.changeBackground(ImageManipulation.lighten(slot2.getBackgroundImage()));
 			dSlot2 = new ItemSlotInformation(frame,"Heals your hero for    100 health.");
 			dSlot2.setLocation(frame.getWidth()-425,frame.getHeight()-400);
-			dSlot2.setVisible(true);	
-	    }	
+			dSlot2.setVisible(true);
+	    }
 		if(e.getSource() == slot3) {
 	        slot3.setBackground(Color.YELLOW);
 	    }
 		if(e.getSource() == slot4) {
-			slot4.setBackground(Color.YELLOW);	    
-		}	
+			slot4.setBackground(Color.YELLOW);
+		}
 		if(e.getSource() == slot5) {
 	        slot5.setBackground(Color.YELLOW);
 	    }
 		if(e.getSource() == slot6) {
-			slot6.setBackground(Color.YELLOW);	    
-		}	
+			slot6.setBackground(Color.YELLOW);
+		}
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		repaint();
 		if(e.getSource() == slot1) {
+			slot1.changeBackground(ImageLoader.load(GlobalConstants.GUI_FILEPATH + slot1.getImageName()));
 			dSlot1.dispose();
 	    }
 		if(e.getSource() == slot2) {
+			slot2.changeBackground(ImageLoader.load(GlobalConstants.GUI_FILEPATH + slot2.getImageName()));
 			dSlot2.dispose();
-		}	
+		}
 		if(e.getSource() == slot3) {
 	        slot3.setBackground(new Color(255,0,0,50));
 	    }
 		if(e.getSource() == slot4) {
-			slot4.setBackground(new Color(255,0,0,50));	    
-		}	
+			slot4.setBackground(new Color(255,0,0,50));
+		}
 		if(e.getSource() == slot5) {
 	        slot5.setBackground(new Color(255,0,0,50));
 	    }
 		if(e.getSource() == slot6) {
-			slot6.setBackground(new Color(255,0,0,50));	    
-		}	
+			slot6.setBackground(new Color(255,0,0,50));
+		}
 	}
 
-	
+
 	@Override
 	public void mousePressed(MouseEvent e) {}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {}
-	
+
 
 }
