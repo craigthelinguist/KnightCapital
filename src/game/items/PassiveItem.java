a53a9a9b52386c0b0ef22d3d73103f8870fd3cc6
craigthelinguist <sinroth@gmail.com>
2014-10-07 23:29:24 +1300

    updates javadocs for items package

--- src/game/items/PassiveItem.java
+++ src/game/items/PassiveItem.java
@@ -2,6 +2,7 @@ package game.items;
 
 /**
  * Passive items sit in your inventory and their effects happen from there.
+ * e.g.: a banner that increases your movement points on overworld 
  * @author craigthelinguist
  *
  */
