2fcaea49b25885e1ac5dee68868655f1162eb7e7
CraigTheLinguist <sinroth@gmail.com>
2014-10-11 20:02:00 +1300

    added herostats and unitstats components

---
+++ src/game/units/UnitStats.java
@@ -0,0 +1,112 @@
+package game.units;
+
+public class UnitStats{
+
+	protected final int baseHealth;
+	protected final int baseDamage;
+	protected final int baseSpeed;
+	protected final int baseArmour;
+
+	protected int buffedDamage;
+	protected int buffedSpeed;
+	protected int buffedArmour;
+	protected int buffedHealth;
+
+	protected int currentHealth;
+
+	public UnitStats(int health, int damage, int speed, int armour){
+		baseHealth = health;
+		baseDamage = damage;
+		baseSpeed = speed;
+		baseArmour = armour;
+		currentHealth = baseHealth;
+		buffedDamage = 0;
+		buffedSpeed = 0;
+		buffedArmour = 0;
+		buffedHealth = 0;
+	}
+
+	/**
+	 *
+	 * is this needed for XML loading? - Aaron
+	 *
+	 *
+	 * Set the base value for some stat.
+	 * @param stat: stat whose base value you will change.
+	 * @param value: new value of the stat.
+
+	public void setBase(Stat stat, int value){
+		if (stat == Stat.HEALTH) baseHealth = value;
+		else if (stat == Stat.DAMAGE) baseDamage = value;
+		else if (stat == Stat.SPEED) baseSpeed = value;
+		else if (stat == Stat.ARMOUR) baseArmour = value;
+		else{
+			throw new RuntimeException("you're setting an unknown stat for a unit");
+		}
+	}
+	*/
+
+	/**
+	 * Set the buffed value for some stat.
+	 * @param stat: stat whose buffed value you will change.
+	 * @param value: new value of the stat.
+	 */
+	public void setBuff(Stat stat, int value){
+		if (stat == Stat.HEALTH) buffedHealth = value;
+		else if (stat == Stat.DAMAGE) buffedDamage = value;
+		else if (stat == Stat.SPEED) buffedSpeed = value;
+		else if (stat == Stat.ARMOUR) buffedArmour = value;
+		else{
+			throw new RuntimeException("you're setting an unknown buffedStat for a unit");
+		}
+	}
+
+	/**
+	 * Set the current value for some stat.
+	 * @param stat: stat whose current value you will change.
+	 * @param value: new value of the stat.
+	 */
+	public void setCurrent(Stat stat, int value){
+		if (stat == Stat.HEALTH) currentHealth = value;
+		else{
+			throw new RuntimeException("you're setting an unknown current stat for a unit");
+		}
+	}
+
+	/**
+	 * Get the base value for some stat.
+	 * @param stat: stat whose base value you want.
+	 * @return: base value of that stat.
+	 */
+	public int getBase(Stat stat){
+		if (stat == Stat.HEALTH) return baseHealth;
+		else if (stat == Stat.DAMAGE) return baseDamage;
+		else if (stat == Stat.SPEED) return baseSpeed;
+		else if (stat == Stat.ARMOUR) return baseArmour;
+		else throw new RuntimeException("You're getting an unknown base stat for a unit");
+	}
+
+	/**
+	 * Get the buffed value for some stat.
+	 * @param stat: stat whose buffed value you want.
+	 * @return: buffed value of that stat.
+	 */
+	public int getBuff(Stat stat){
+		if (stat == Stat.HEALTH) return buffedHealth;
+		else if (stat == Stat.DAMAGE) return buffedDamage;
+		else if (stat == Stat.SPEED) return buffedSpeed;
+		else if (stat == Stat.ARMOUR) return buffedArmour;
+		else throw new RuntimeException("You're getting an unknown base stat for a unit");
+	}
+
+	/**
+	 * Get the current value for some stat.
+	 * @param stat: stat whose current value you want.
+	 * @return: current value of that stat.
+	 */
+	public int getCurrent(Stat stat){
+		if (stat == Stat.HEALTH) return currentHealth;
+		else throw new RuntimeException("You're getting an unknown base stat for a unit");
+	}
+
+}
