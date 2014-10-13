c4986be3e45f09256cb4c28c0776834a27314f08
EwanMoshi <moshiewan@live.com>
2014-10-13 14:59:32 +1300

    added images for the leave city button

--- src/GUI/town/TownButtonPanel.java
+++ src/GUI/town/TownButtonPanel.java
@@ -4,27 +4,38 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import tools.Constants;
+import tools.ImageLoader;
+import GUI.CustomButton;
 import controllers.TownController;
 
 public class TownButtonPanel extends JPanel{
 
 	private TownController controller;
-	private JButton button_leave;
+	private CustomButton button_leave;
 	
 	protected TownButtonPanel(Dimension dimensions, TownController townController) {
 		this.controller = townController;
 		this.setPreferredSize(dimensions);
-		button_leave = new JButton("Leave");
+		
+		/*Declare and initialize the images for the buttons */
+		BufferedImage closeTownDefault = ImageLoader.load(Constants.GUI_TOWN_BUTTONS + "closeButton.png");
+		BufferedImage closeTownPressed = ImageLoader.load(Constants.GUI_TOWN_BUTTONS + "closeButtonClicked.png");
+		BufferedImage closeTownHover = ImageLoader.load(Constants.GUI_TOWN_BUTTONS + "closeButtonHover.png");
+		button_leave = new CustomButton(closeTownDefault, closeTownPressed, closeTownHover);
+		
 		button_leave.setPreferredSize(new Dimension(80,50));
 		button_leave.addActionListener(new ActionListener(){
-
+			
+			
 			@Override
 			public void actionPerformed(ActionEvent event) {
-				controller.buttonPressed(button_leave.getText());
+				controller.buttonPressed("leave");
 			}
 		
 		});
