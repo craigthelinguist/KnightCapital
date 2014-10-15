5938253417130efecfd99002cbdc0428343dd880
CraigTheLinguist <sinroth@gmail.com>
2014-10-16 01:19:34 +1300

    this one goes out to all my teachers who never said i'd amount to nothing

--- src/player/Player.java
+++ src/player/Player.java
@@ -2,18 +2,23 @@ package player;
 
 import java.io.Serializable;
 
+/**
+ * Represents a player in an instance of a game being played on the world.
+ * @author Neal, Solomon, Aaron
+ */
 public class Player implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+
 	// name = name of the player
 	// slot = player 1, player 2, etc.
 	public final String name;
 	public final int slot;
+
+	// how much gold this player has
 	private int gold;
 
+	// the colour for the player depends on their slot
 	public static final int NEUTRAL = 0;
 	public static final int RED = 1;
 	public static final int BLUE = 2;
@@ -33,6 +38,10 @@ public class Player implements Serializable{
 
 	public int getSlot(){return this.slot;}
 
+	/**
+	 * Return the colour of this player as a string.
+	 * @return: string
+	 */
 	public String getColour(){
 		if (slot == 1) return "red";
 		else if (slot == 2) return "blue";
@@ -41,15 +50,27 @@ public class Player implements Serializable{
 		else throw new RuntimeException("illegal slot for player " + name + ", slot was " + slot);
 	}
 
+	/**
+	 * Return this player's gold.
+	 * @return: int
+	 */
 	public int getGold() {
 		return this.gold;
 	}
 
+	/**
+	 * Increase this player's gold.
+	 * @param amount: amount by which to increase.
+	 */
 	public void increaseGold(int amount) {
 		if (amount <= 0) return;
 		this.gold += amount;
 	}
 
+	/**
+	 * Decrease this player's gold.
+	 * @param amount: amount by which to decrease.
+	 */
 	public void decreaseGold(int amount){
 		if (amount <= 0) return;
 		this.gold -= amount;
