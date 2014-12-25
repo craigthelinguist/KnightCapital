59602734ec5a6ff0b3785c612fad5078cfe8d9ef
unknown <sinroth@gmail.com>
2014-12-25 15:11:50 +1300

    simplified GameDialog considerably

--- src/GUI/world/GameDialog.java
+++ src/GUI/world/GameDialog.java
@@ -1,62 +1,123 @@
 package GUI.world;
 
-
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
-import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 import tools.Constants;
 import tools.ImageLoader;
+import GUI.world.CustomButton;
 
+public class GameDialog extends JDialog{
 
-/**
- * This class extends JDialog and contains a layeredPane to display a border and a panel
- * for the buttons.
- *
- * @author Ewan Moshi
- *
- */
-public class GameDialog extends JDialog {
-
-	private DialogPanel panel;
-	private String FILENAME = Constants.GUI_BUTTONS;
-
-	public GameDialog (MainFrame frame,String msg) {
-		super(frame,true);
-		this.setSize(400,374); //width, height
-		this.setUndecorated(true); //removes the border
-		// TODO: if i don't uncommenet this i can't exit the frame.... need to do this to test end turn
-		// maybe there should be butotn on the gamedialog u click ok and it exits dialog - Aaron
-
-        this.setLocationRelativeTo(frame); //set location of dialog relative to frame
-
-	    DialogLayeredPane dLayeredPane = new DialogLayeredPane(this,msg);
-		this.add(dLayeredPane);
-		this.setResizable(false);
-		this.setVisible(true);
+	private JButton button;
+	private JTextPane textPane;
+	
+	public GameDialog(JFrame main, String message){
+		super(main,true); // true arg makes this Dialog the focus
+		GameDialog gameDialog = this;
+		gameDialog.setLayout(new BorderLayout());
+		gameDialog.setUndecorated(true); //true means borderless window
+			
+		// set up components
+		makeTextPane("\n"+message);
+		makeButton();
+		gameDialog.add(textPane, BorderLayout.CENTER);
+		gameDialog.add(button, BorderLayout.SOUTH);
+		
+		// put padding on bottom to make button look netter
+		JPanel contentPane = (JPanel)gameDialog.getContentPane();
+		contentPane.setBorder(new EmptyBorder(0,0,20,0));
+		contentPane.setBackground(Color.BLACK);
+		
+		gameDialog.setSize(300,250);
+		gameDialog.setLocationRelativeTo(main);
+		gameDialog.setResizable(false);
+		gameDialog.setVisible(true);
 	}
-
-
+	
+	/**
+	 * Make and set up the JTextPane to display on this dialog.
+	 * @param msg: the string to display in the JTextPane
+	 */
+	private void makeTextPane(String msg){
+		
+		// make text pane, set properties
+		this.textPane = new JTextPane();
+		textPane.setEditable(false);
+		
+		// set font, text colour, and message
+		textPane.setForeground(Color.WHITE);
+		Font font = new Font("Franklin Gothic Medium", Font.BOLD, 20);
+		textPane.setFont(font);
+		textPane.setText(msg);
+		
+		// background colour will be derived from parent contanier
+		textPane.setOpaque(false);
+		
+		// stops you being able to higlight the text
+		textPane.setHighlighter(null);
+		
+		// align text in centre
+		StyledDocument doc = textPane.getStyledDocument();
+		SimpleAttributeSet center = new SimpleAttributeSet();
+		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+		doc.setParagraphAttributes(0,  doc.getLength(), center, false);
+		
+		// pad left and right margins a bit
+		textPane.setMargin(new Insets(0,10,10,0));
+		
+	}
+	
+	/**
+	 * Make and set up the button to display on this dialog.
+	 * @return
+	 */
+	private void makeButton(){
+		BufferedImage buttonDefault = ImageLoader.load(Constants.GUI_FILEPATH + "dialogConfirmButton.png");
+		BufferedImage buttonPressed = ImageLoader.load(Constants.GUI_FILEPATH + "dialogConfirmButtonPressed.png");
+		this.button = new CustomButton(buttonDefault, buttonPressed, buttonDefault);
+		button.setBackground(Color.BLACK);
+	
+		// this button disposes of the GameDialog on click
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				GameDialog.this.dispose();
+			}
+		});
+		
+	}
+	
 	/**
 	 * Test method for the dialog
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		try {
-			GameDialog dialog = new GameDialog(new MainFrame(),"Rescue Neal, he was last seen inside the wizard's tower");
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		JFrame frame = new JFrame();
+		frame.setSize(400,400);
+		frame.pack();
+		frame.setVisible(true);
+		new GameDialog(frame, "hello world. How are yuo? this is my test for a raelly long string. FTP");
+		frame.dispose();
+		System.exit(0);
 	}
 
+	
 }
